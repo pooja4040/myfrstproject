24b543ad95086eb14f75ef1f1d365f9ee95a8e1b
edavelli.poojareddy <edavelli.poojareddy@LP-PF2XF41Y.HCLT.CORP.HCL.IN>
2021-08-27 18:41:30 +0530

    new project commited

---
+++ automationgit/src/test/java/sample/NewTest.java
@@ -0,0 +1,20 @@
+package sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+
+	  WebDriver driver;
+	  
+	  driver=new ChromeDriver();
+  driver.get("https://www.google.co.in/");
+  
+  
+  }
+}
